83bba7d99fb0cf94ced4ca9812b2fc888a6e9602
totorowg <wangchingying@gmail.com>
2017-12-28 15:18:58 +0800

    Initial commit

---
+++ app/src/main/java/com/cyw/a2017022802_1/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.cyw.a2017022802_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+   }
+
+}
---
+++ app/src/main/java/com/cyw/a2017022802_1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.cyw.a2017022802_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.button);
+    }
+    public void click1(View v)
+    {
+        Intent it=new Intent();
+        it.setAction("DD123123");
+        //System.out.println("here"+it);
+        it = Intent.createChooser(it, "請選擇一個App 來執行!");
+        startActivity(it);
+
+    }
+}
